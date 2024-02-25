1cde650cd817de230bd9dc99b937dc2f6e71959c
gustavo alves <gustavo.twister10@gmail.com>
2024-02-25 17:51:18 -0300

    created component menu-bar

---
+++ src/app/menu-bar/menu-bar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MenuBarComponent } from './menu-bar.component';
+
+@NgModule({
+  declarations: [
+    MenuBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+})
+export class MenuBarModule { }
