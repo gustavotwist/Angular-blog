50bf610666c7f94fe3614d5643d377560d043048
gustavo alves <gustavo.twister10@gmail.com>
2024-02-26 22:01:25 -0300

    Ajustada navBar

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -7,9 +7,18 @@ import { Component } from '@angular/core';
 })
 
 export class MenuBarComponent {
-
+  public menuItem = [
+    { texto: 'Home', caminho: '/home' },
+    { texto: 'Sobre', caminho: '/sobre' },
+    { texto: 'Contato', caminho: '/contato' }
+  ];
+  public activeItem = 0;
   public isExpansive = false
   public expansive(isExpansive: boolean) {
     this.isExpansive = isExpansive
+  }
+
+  setActive(index: number): void {
+    this.activeItem = index; 
   }
 }
